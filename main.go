035a6c6fd6ad9125b878a36be2ec684de9136be0
torbenconto <torbenmconto@gmail.com>
2025-04-04 23:13:35 -0400

    additional features

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/torbenconto/plutus/v2"
@@ -29,8 +30,71 @@ var (
 	neutralChangeColor  = "#454545"
 )
 
+var sectors = map[string][]string{
+	"Finance": {
+		"JPM", "GS", "BAC", "WFC", "C", "AXP", "BRK.B", "BLK", "V", "SCHW", "XLF",
+	},
+}
+
+type SectorOverview struct {
+	AverageChangePercent     float64
+	Average52wkChangePercent float64
+}
+
+func getSectorData() (map[string]SectorOverview, error) {
+	sectorOverview := make(map[string]SectorOverview)
+	var wg sync.WaitGroup
+	mu := sync.Mutex{} // To ensure safe concurrent updates to sectorOverview
+
+	for sector, tickers := range sectors {
+		wg.Add(1)
+		go func(sector string, tickers []string) {
+			defer wg.Done()
+
+			var totalChangePercent, total52wkChangePercent float64
+			var count int
+
+			for _, ticker := range tickers {
+				data, err := plutus.GetQuote(ticker)
+				if err != nil {
+					fmt.Printf("Error fetching data for %s: %v\n", ticker, err)
+					continue
+				}
+				totalChangePercent += data.RegularMarketChangePercent
+				total52wkChangePercent += data.FiftyTwoWeekChangePercent
+				count++
+			}
+
+			if count > 0 {
+				mu.Lock()
+				sectorOverview[sector] = SectorOverview{
+					AverageChangePercent:     totalChangePercent / float64(count),
+					Average52wkChangePercent: total52wkChangePercent / float64(count),
+				}
+				mu.Unlock()
+			}
+		}(sector, tickers)
+	}
+
+	wg.Wait()
+
+	return sectorOverview, nil
+}
+
 func main() {
 	var boxes []string
+
+	sectorDataChan := make(chan map[string]SectorOverview)
+	errChan := make(chan error)
+
+	go func() {
+		sectorData, err := getSectorData()
+		if err != nil {
+			errChan <- err
+			return
+		}
+		sectorDataChan <- sectorData
+	}()
 
 	for _, ticker := range marketIndicators {
 		quote, err := plutus.GetQuote(ticker)
@@ -65,4 +129,15 @@ func main() {
 	layout := containerStyle.Render(content)
 
 	fmt.Println(layout)
+
+	select {
+	case sectorData := <-sectorDataChan:
+		// Process the sectorData
+		for sector, summary := range sectorData {
+			fmt.Println(sector, summary.AverageChangePercent)
+		}
+
+	case err := <-errChan:
+		panic(err)
+	}
 }
